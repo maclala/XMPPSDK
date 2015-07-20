36263540227e12f17ecfed84a635fff5a33dc214
丁海伟 <dhw@mi.local>
2015-07-20 08:46:30 +0800

    Initial Commit

---
+++ XMPPSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMPPSDK
+//
+//  Created by 丁海伟 on 15-7-20.
+//  Copyright (c) 2015年 dhwheavy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMPPSDK/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMPPSDK
+//
+//  Created by 丁海伟 on 15-7-20.
+//  Copyright (c) 2015年 dhwheavy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
